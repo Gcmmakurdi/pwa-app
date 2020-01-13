1a3333a8d3663fa9ec78c5b1bc2c0b22bbe00ef1
Adie Chris <git config --global user.email>
2020-01-13 18:20:20 +0100

    added about section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,12 @@ import { environment } from '../environments/environment';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { CarouselComponent } from './carousel/carousel.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarouselComponent,
     NavBarComponent,
-    AboutUsComponent
   ],
   imports: [
     BrowserModule,
