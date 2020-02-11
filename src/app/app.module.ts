5cbc0fa9b05d7884f364debfb72d2d0d9f856d37
Adie Chris <git config --global user.email>
2020-02-11 01:30:32 +0100

    landing page changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,24 @@ import { environment } from '../environments/environment';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { CarouselComponent } from './carousel/carousel.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {faHome, faGripVertical} from '@fortawesome/free-solid-svg-icons';
+import { EventsComponent } from './events/events.component';
 
+library.add(faFacebook, faHome, faGripVertical);
 @NgModule({
   declarations: [
     AppComponent,
     CarouselComponent,
     NavBarComponent,
+    EventsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FontAwesomeModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     CarouselModule.forRoot()
   ],
